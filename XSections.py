d35a9f20fc43865025f062184d92dde9f2f4fa94
Michael Vander Wal <vandermwal@gmail.com>
2020-01-08 14:24:02 -0500

    This class is supposed to be used to hold the cross-section data. It is currently only implemented for discrete data. I might not have actually submitted a saved file last time.

--- XSections.py
+++ XSections.py
@@ -4,5 +4,34 @@ Created on Wed Jan  8 14:04:49 2020
 
 @author: Michael Vander Wal
 """
+import numpy as np
+# import TransportConstants as TC
+
+class XSection():
+    xSection = None
+    energyValues = None
+    discrete = True
+    def __init__(self,crossSection,energy,discrete = True):
+        self.xSection = np.array(crossSection)
+        self.energyValues = np.array(energy)
+        self.discrete = discrete
 
 
+    def getXSection(self,energy):
+        if self.discrete:
+            return self.interp(energy)
+        else:
+            return self.interp(energy) #need to change to a function in future
+        return
+
+    def interp(self,xStar):
+        i = 0
+        length = len(self.energyValues)
+        while i < length and self.energyValues[i] <= xStar:
+            i += 1
+        x1 = self.energyValues[i-1]
+        x2 = self.energyValues[i]
+        y1 = self.xSection[i-1]
+        y2 = self.xSection[i]
+        ystar = ((xStar-x1)/(x2-x1))*(y2-y1)+y1
+        return ystar
