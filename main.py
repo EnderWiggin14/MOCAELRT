59f8b48abb02c731263c7f0aa1e7f4da81441309
Michael Vander Wal <vandermwal@gmail.com>
2020-01-09 14:27:14 -0500

    Restructured main() to more closely resemble what order everything will need to be in to set up a simulation.

--- main.py
+++ main.py
@@ -19,15 +19,37 @@ import TransportConstants
 
 
 def main():
-    PM = ParticleManager(['electron'],[100])
+    # Step 1: Initialize ParticleManager
+    PM = ParticleManager()
+
+    # Step 2: Generate Distributions
+
+    # Step 2: Add Materials
     matMan = Material.MaterialManager()
-    mat = Material.Material()
-    distA = Distribution.DiscreteDist()
-    distB = Distribution.ContinuousDist()
-    geo = Geometry.Geometry()
+    mat = Material.Material(14)
+
+    # Step 3: Add Geometry
     geoMan = Geometry.GeometryManager()
+    geo = Geometry.Geometry()
+    geoMan.addGeometry(geo)
+
+    PM.addGeometry(geoMan)
+
+    # Step 4: Add Source Particles
     soMan = Source.SourceManager()
     so = Source.Source()
+    so.setLocation()
+    so.setEnergy()
+    so.setDirection()
+    so.setParticleType()
+    so.setPopulation()
+    soMan.addSource(so)
+
+    PM.addParticles(source = soMan)
+
+
+
+
     return
 
 
