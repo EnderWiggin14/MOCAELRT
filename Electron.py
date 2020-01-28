Corrected rmsvariance calculation in heat map. Started adding a better ionization energy calculation to DataGenerators

Electron.py
@@ -14,7 +14,7 @@ import Distribution
 class Electron(Particle):
 # class Electron(Particle.Particle):
 
-    def __init__(self, loc = [0.,0.,0.], direc = [0.,0.,1.], enrg = 100.,pid = np.random.randint(0,9999999) ):
+    def __init__(self, loc = [0.,0.,0.], direc = [0.,0.,1.], enrg = 100., weight = 1. ,pid = -1 ):
         Particle.__init__(self,'electron',loc,direc,enrg)
         return
 

Particle.py
@@ -27,7 +27,7 @@ class Particle(metaclass=abc.ABCMeta):
     curCell = None
     newWgt = 1.
 
-    def __init__(self,species='electron',loc = [0.,0.,0.], direc = [0.,0.,1.], enrg = 100.,pid = -1):
+    def __init__(self,species='electron',loc = [0.,0.,0.], direc = [0.,0.,1.], enrg = 100.,weight = 1.,pid = -1):
         if isinstance(species,str):
             self.particleType = self.enumMap(species)
         else:
@@ -35,7 +35,8 @@ class Particle(metaclass=abc.ABCMeta):
         self.loc = np.array(loc)
         self.direc = np.array(direc)
         self.E = self.energyConvert(enrg)
-        self.wgt = 1.
+        self.wgt = weight
+        self.newWgt = weight
         self.ID = pid
         return
 

Tally.py
@@ -142,9 +142,9 @@ class HeatMap(Tally):
 
     # def printObjectToFile(self,fileName = "HeatMap.npy"):
     #     np.save(fileName,self)
-    def rmsVariance(self):
+    def rmsVariance(self,initialPop):
         variance = np.zeros(self.mesh.shape)
-        variance = self.squaredMesh-self.mesh**2
+        variance = self.squaredMesh/initialPop-(self.mesh/initialPop)**2
         rms = variance**2
         variance = variance.flatten()
         count = 0

main.py
@@ -70,7 +70,7 @@ def main():
     so.setEnergy(1000)
     so.setDirection(np.array([0.,0.,1.]))
     so.setParticleType()
-    so.setPopulation(1000)
+    so.setPopulation(10000)
     soMan.addSource(so)
 
     PM.addParticles(source = soMan)
@@ -86,8 +86,8 @@ def main():
     tal.createGraphic(projection = 'xy')
     tal.createGraphic(projection = 'yz')
     tal.createGraphic(projection = 'xz')
-    rms = tal.rmsVariance()
-    print(rms)
+    rms = tal.rmsVariance(10000)
+    print("Root Mean Square variance: ",rms)
     elapsedTime = timerStop-timerStart
     print("Elapsed Time: ",elapsedTime)
     print("FOM: ",1/(rms*elapsedTime))